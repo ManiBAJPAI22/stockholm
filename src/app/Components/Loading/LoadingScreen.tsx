1febca4c98a71944e9a5674936be1aa80f7c9e96
Mani_B <manicode22@gmail.com>
2024-06-10 15:19:44 +0530

    added Animation to Loading 💫

--- src/app/Components/Loading/LoadingScreen.tsx
+++ src/app/Components/Loading/LoadingScreen.tsx
@@ -1,22 +1,10 @@
 import React from "react";
+import Spline from "@splinetool/react-Spline";
 
 export default function LoadingScreen() {
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(255, 255, 255, 0.9)",
-        zIndex: 1000,
-      }}
-    >
-      <div>Loading...</div>
+    <div>
+      <Spline scene="https://prod.spline.design/rs-F5OeZuE11GFEO/scene.splinecode"></Spline>
     </div>
   );
 }
