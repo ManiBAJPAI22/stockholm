12a5d656f6092e5870ee81fd9b2631874592b207
Mani_B <manicode22@gmail.com>
2024-06-10 11:31:09 +0530

    header modified

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,25 +1,36 @@
+import React from "react";
+
 export default function Header() {
   return (
-    <div
-      style={{
-        backgroundColor: "#FDF8BF",
-      }}
-    >
-      <div className="flex px-8 py-3 justify-between">
-        <div className="font-bold text-3xl flex justify-center items-center">
-          Stockholm.
-        </div>
-        <div className="text-2xl justify-center">About</div>
-        <div className="text-2xl justify-center">Docs</div>
-        <div
-          style={{
-            backgroundColor: "white",
-            border: "2px solid black",
-            padding: "0.1rem",
-            borderRadius: "1px",
-          }}
-        >
-          <div className="font-bold justify-center">Launch App</div>
+    <div className="glass-header">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink">
+              <a href="/" className="font-bold text-2xl text-black">
+                Stockholm.
+              </a>
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-center space-x-4">
+              <a href="/" className="text-black rounded-lg p-2">
+                About
+              </a>
+              <a href="/" className="text-black rounded-lg p-2">
+                Docs
+              </a>
+              <a href="/" className="text-black rounded-lg p-2">
+                Team
+              </a>
+              <a
+                href="/"
+                className="font-bold  text-xl text-black hover:bg-black hover:text-white rounded-lg p-2 "
+              >
+                Connect
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
